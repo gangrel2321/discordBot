a99538ddc5c037e1e180f7134003fd6d8a217002
jspang3 <jspang3@hotmail.com>
2019-03-30 18:48:21 -0400

    Fixed promise loop, added file training file creation

--- commands/allocateText.js
+++ commands/allocateText.js
@@ -4,28 +4,38 @@ module.exports = {
 	description: `Loads current text for learning.`,
 	execute(message, args) {
         var lastId = message.channel.lastMessageID
-        var done = 0
-        while(!done)
+        const fs = require('fs')
+        const path = './messageTrain.txt'
+        try {
+            if (fs.existsSync(path))
+                fs.writeFile('messageTrain.txt', '', (err) => { if(err) throw err; });
+            else fs.closeSync(fs.openSync('./messageTrain.txt', 'w'));
+        } catch(err) { console.error(err) }
+
+        Promise.resolve().then(function resolver() 
         {
-            done = message.channel.fetchMessages( { limit : 10, before: lastId} ).then(messages => 
+            return message.channel.fetchMessages( { limit : 100, before: lastId} ).then(messages => 
                 {
-                    //console.log(`Done: ${done}`);
-                    if(!messages.size) { return 1;};
+                    if(!messages.size) { return Promise.reject('Completed.');};
                     //add save to sql database later
-                    let file = fs.createWriteStream('messageTrain.txt');
-                    file.on('error', err => { console.log(`Error opening training file.`)});
+                    //let file = fs.createWriteStream('messageTrain.txt');
+                    //file.on('error', err => { console.log(`Error opening training file.`)});
                     let data = messages.array();
                     data.sort( (a,b) => { return a.createdAt < b.createdAt ? -1 : 1; });
-                    data.forEach(mess => { file.write(`${mess.content}\n`); });
-                    file.end();
+                    data.forEach(mess => { 
+                        //file.write(`${mess.content}\n`); 
+                        fs.appendFile('messageTrain.txt', `${mess.content}\n`, (err) => {  
+                            if (err) throw err;
+                        })
+                    });
+                    //file.end();
                     if(data.length) lastId = data[0].id;
-                    else { return 1;};
+                    else { return Promise.reject('Completed.');};
                     console.log(`Logged ${messages.size} messages`);
                     return 0;
                 }
-            );
-            console.log(`Done: ${done}`);
-        }
+            ).then(resolver); 
+        }).catch((error) => { console.log(error); } );
         
 	},
 }
